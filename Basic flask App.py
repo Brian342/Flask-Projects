0570db6e6b758ff1316038b7cfab791c79bee784
Brian kimanzi <migelbrian3@gmail.com>
2025-10-25 09:05:06 +0300

    new code execution

--- Basic flask App.py
+++ Basic flask App.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 # declaration of the route
+# @app.route('/')
+# def index():
+#
+#     return '<h1>Hello World</h1>'
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p> Your Browser is {}</p>'.format(user_agent)
 
 
 app.add_url_rule('/', 'index', index)  # the URL, the endpoint name, and the view function
