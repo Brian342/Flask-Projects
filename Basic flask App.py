0db8f19bc4ac8625f435bf8f5081ee9aafd559ed
Brian kimanzi <migelbrian3@gmail.com>
2025-10-25 08:37:11 +0300

    new code execution

--- Basic flask App.py
+++ Basic flask App.py
@@ -7,3 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World</h1>'
+
+
+app.add_url_rule('/', 'index', index)  # the URL, the endpoint name, and the view function
+
+
+# urls with username
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
