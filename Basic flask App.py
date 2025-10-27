717683e953b8f0e35b390931057ca27e468c7062
Brian kimanzi <migelbrian3@gmail.com>
2025-10-27 08:54:53 +0300

    new code version

--- Basic flask App.py
+++ Basic flask App.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, abort
 
 app = Flask(__name__)
 
@@ -8,10 +8,17 @@ app = Flask(__name__)
 # def index():
 #
 #     return '<h1>Hello World</h1>'
+# @app.route('/')
+# def index():
+#     user_agent = request.headers.get('User-Agent')
+#     return '<p> Your Browser is {}</p>'.format(user_agent)
+
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p> Your Browser is {}</p>'.format(user_agent)
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    redirection = "http://www.example.com"
+    return response, redirection
 
 
 app.add_url_rule('/', 'index', index)  # the URL, the endpoint name, and the view function
@@ -21,3 +28,11 @@ app.add_url_rule('/', 'index', index)  # the URL, the endpoint name, and the vie
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+
+@app.route('/user/<id>')
+def get_user(id):
+    # user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, {}!</h1>'.format(user.name)
