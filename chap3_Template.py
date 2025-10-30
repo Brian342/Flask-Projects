388db62617ac8e23cc61760a9b2b3870f0ceaa9c
Brian kimanzi <migelbrian3@gmail.com>
2025-10-30 23:22:46 +0300

    new code version

--- chap3_Template.py
+++ chap3_Template.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', user=name)
+    return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
