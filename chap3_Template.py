1df59b9420dd91b218cbc02a48a34f38426e4404
Brian kimanzi <migelbrian3@gmail.com>
2025-10-30 23:04:52 +0300

    new code update

--- chap3_Template.py
+++ chap3_Template.py
@@ -16,5 +16,15 @@ def user(name):
     return render_template('user.html', user=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(505)
+def internal_server_error(e):
+    return render_template('505.html'), 505
+
+
 if __name__ == "__main__":
     app.run(debug=True)
