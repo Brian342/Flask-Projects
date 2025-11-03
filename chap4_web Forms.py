6536e6811bf01dffdc13f7d2f7148d884e67de89
Brian kimanzi <migelbrian3@gmail.com>
2025-11-04 02:38:28 +0300

    new code update

--- chap4_web Forms.py
+++ chap4_web Forms.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 Bootstrap = Bootstrap(app)
-moment = Moment(app)
+
+
+class nameForm(FlaskForm):
+    name = StringField('What is your Name?', validators=[DataRequired])
+    submit = SubmitField('Submit')
 
 
 if __name__ == "__main__":
