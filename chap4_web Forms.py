905544f4e9b9c73689dc7e32b80a85ecda85ba2c
Brian kimanzi <migelbrian3@gmail.com>
2025-11-04 02:27:11 +0300

    new code update

---
+++ chap4_web Forms.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+Bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
